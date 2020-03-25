4e053e7b8c6d0e0c3352209485418d8ca69379c9
xcixor <pndungu54@gmail.com>
2020-03-25 18:59:52 +0300

    feat(doctor registration): add ability to add images and user location

--- app/src/main/java/com/ecareuae/e_care/FirebaseUtil.java
+++ app/src/main/java/com/ecareuae/e_care/FirebaseUtil.java
@@ -2,11 +2,16 @@ package com.ecareuae.e_care;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class FirebaseUtil {
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
     private static FirebaseUtil firebaseUtil;
+    private static StorageReference mStorageReference;
+    public static FirebaseStorage mStorage;
+//    private static String objectId;
 
     private FirebaseUtil(){}
 
@@ -14,6 +19,12 @@ public class FirebaseUtil {
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference();
         return mDatabaseReference;
+    }
+
+    public static StorageReference getmStorageReference(){
+        mStorage = FirebaseStorage.getInstance();
+        mStorageReference = mStorage.getReference().child("user_images");
+        return mStorageReference;
     }
 
 }
